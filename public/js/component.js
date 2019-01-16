d596d94085cae6ae2f481dd2572eb2f487f3a802
DeltaChrome <98hawkeye@gmail.com>
2019-01-16 16:09:51 -0500

    more cool stuff
    lights
    camera
    action

---
+++ public/js/component.js
@@ -0,0 +1,19 @@
+AFRAME.registerComponent("test-component", {
+
+schema:
+{
+
+message: {type: 'string', default: "no message"},
+name: {type: 'string', default: ""}
+
+
+},
+
+init: function ()
+{
+    console.log(this.data.message);
+    console.log(this.data.name);
+    
+}
+
+});
