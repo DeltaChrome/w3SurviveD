98df489f85c9443eff8beffaf6ae080d5e158e1b
JonahJana <46599996+JonahJana@users.noreply.github.com>
2019-03-06 14:09:01 -0500

    implementing websocket stuff from culearn
    we out here frankensteining bois

--- app.js
+++ app.js
@@ -12,6 +12,47 @@ const server = http.createServer(app);
     res.sendFile(__dirname + 'public/index.html');
 })
 
+//-----------------------------------
+
+//routes
+app.get('/color', function(req,res) {
+    res.sendFile(__dirname + '/public/color.html');
+});
+
+app.get('/controller', function(req,res) {
+    res.sendFile(__dirname + '/public/controller.html');
+});
+
+//websocket stuff
+socketIO.on('connection', function(socket) {
+    console.log(socket.id + ' has connected!');
+
+    socket.on('disconnect', function(data) {
+        console.log(socket.id + ' has disconnected');
+    });
+
+    //custom events
+    //socket = one client
+    //socketIO.sockets = all clients
+    socket.on('red', function(data) {
+        console.log('red event heard');
+        socketIO.sockets.emit('color_change', {r:255, g:0, b:0});
+    });
+
+    socket.on('green', function(data) {
+        console.log('green event heard');
+        socketIO.sockets.emit('color_change', {r:0, g:255, b:0});
+    });
+
+    socket.on('blue', function(data) {
+        console.log('blue event heard');
+        socketIO.sockets.emit('color_change', {r:0, g:0, b:255});
+    });
+});
+
+
+//-----------------------------------------
+
 server.listen(LISTEN_PORT);
 
 console.log('Listening to port' + LISTEN_PORT);
