68f20af581584d80679d7894e4a52f098f653eb3
JonahJana <46599996+JonahJana@users.noreply.github.com>
2019-03-06 16:16:37 -0500

    adding assets
    ads

---
+++ public/js/itemSpawn.js
@@ -0,0 +1,16 @@
+AFRAME.registerComponent("SpawnItems-component", {
+   
+    init: function ()
+    {
+        let scene = document.querySelector('a-scene');
+
+        let entity = document.createElement('a-entity');
+            entity.setAttribute('position', {x:0,y:0,z:0});
+            entity.setAttribute('obj-model', 'obj: #Axe_1-obj');
+            entity.setAttribute('material', 'mtl: #Axe_1-mat');
+            entity.setAttribute('scale', '100 100 100');
+
+            scene.appendChild(entity);
+
+    }
+});
