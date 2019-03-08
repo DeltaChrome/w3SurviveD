01a96096ab07e36bc4f03eb085ac5ed086ed55bb
JonahJana <46599996+JonahJana@users.noreply.github.com>
2019-03-08 09:45:08 -0500

    axe in the game
    it big but its in there

--- public/js/runtime.js
+++ public/js/runtime.js
@@ -2,8 +2,8 @@ AFRAME.registerComponent("runtime", {
     schema:
     {
 
-    PlayerGX: {type: int, default: 5},
-    PlayerGY: {type: int, default: 5}
+    PlayerGX: {type: 'number', default: 5},
+    PlayerGY: {type: 'number', default: 5}
 
 
     },
