3cdb6a2d6ee92c9c24cc7b0c570a559d7738e4bf
JonahJana <46599996+JonahJana@users.noreply.github.com>
2019-03-13 16:26:36 -0400

    backpack and axe are spawning props

--- public/js/itemSpawn.js
+++ public/js/itemSpawn.js
@@ -1,4 +1,4 @@
-AFRAME.registerComponent("SpawnItems-component", {
+AFRAME.registerComponent("spawnItems-component", {
    
     init: function ()
     {
@@ -6,11 +6,10 @@ AFRAME.registerComponent("SpawnItems-component", {
 
         let entity = document.createElement('a-entity');
             entity.setAttribute('position', {x:0,y:0,z:0});
-            entity.setAttribute('obj-model', 'obj: #Axe_1-obj');
-            entity.setAttribute('material', 'mtl: #Axe_1-mat');
-            entity.setAttribute('scale', '100 100 100');
+            entity.setAttribute('obj-model', 'obj: #Backpack_1-obj');
+            entity.setAttribute('material', 'mtl: #Backpack_1-mat');
+            entity.setAttribute('scale', '1 1 1');
 
             scene.appendChild(entity);
-
     }
 });
